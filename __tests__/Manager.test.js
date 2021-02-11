4467da645daecf95cc35aa768ac9c7893fc6859b
Cody Kimb <codykimbell@gmail.com>
2021-02-11 14:41:39 -0600

    Team Name

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -2,17 +2,18 @@ const Manager = require('../lib/Manager');
 
 test('creates manager object', () => {
 
-    const manager = new Manager("Cody", "368323", "codykimb@me.com", "420");
+    const manager = new Manager("Cody", "123456", "codykimb@me.com", "420", "Team Cody");
 
     expect(manager.name).toBe("Cody");
-    expect(manager.id).toBe("368323");
+    expect(manager.id).toBe("123456");
     expect(manager.email).toBe("codykimb@me.com");
     expect(manager.officeNumber).toBe("420");
+    expect(manager.teamName).toBe("Team Cody");
 
 });
 
 test('gets manager role', () => {
-    const manager = new Manager("Cody", "368323", "codykimb@me.com", "UT");
+    const manager = new Manager("Cody", "123456", "codykimb@me.com", "420", "Team Cody");
 
     expect(manager.getRole()).toBe("Manager")
 })
