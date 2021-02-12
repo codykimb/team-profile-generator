1b40d573f28f089418aef85e1a616c23c7969b3e
Cody Kimb <codykimbell@gmail.com>
2021-02-12 02:59:09 -0600

    comments

--- src/pagetemplate.js
+++ src/pagetemplate.js
@@ -4,7 +4,9 @@ function finishTeam(team) {
     console.log("You have finished building your team!")
 
     const htmlArray = []
-    const htmlBeginning = `
+
+    //HTML TOP
+    const htmlTop = `
     <!DOCTYPE html>
         <html lang="en">
         <head>
@@ -21,50 +23,53 @@ function finishTeam(team) {
             </div>
             <div class="card-container">
             `
-            htmlArray.push(htmlBeginning);
-
-            for (let i = 0; i < team.length; i++) {
-                let memberCards = `
-                <div class="member-card">
-                    <div class="card-top">
-                        <h2>${team[i].name}</h2>`
-                        if (team[i].getRole() === "Manager") {
-                            memberCards += `<h2><i class="fas fa-user-tie"></i>`
-                        }
-                        if (team[i].getRole() === "Engineer") {
-                            memberCards += `<h2><i class="fas fa-code-branch"></i>`
-                        }
-                        if (team[i].getRole() === "Intern") {
-                            memberCards += `<h2><i class="fas fa-graduation-cap"></i>`
-                        }
-                        memberCards += ` ${team[i].getRole()}</h2>
-                    </div>
-                    <div class="card-bottom list-group">
-                        <li>Employee ID: ${team[i].id}</li>
-                        <li>Email: <a href="mailto:${team[i].email}">${team[i].email}</a></li>
-                        `
-                if (team[i].officeNumber) {
-                    memberCards += `<li>Office Number: ${team[i].officeNumber}</li>`
+            htmlArray.push(htmlTop);
+            
+    //  MEMBER CARDS
+    for (let i = 0; i < team.length; i++) {
+        let memberCards = `
+        <div class="member-card">
+            <div class="card-top">
+                <h2>${team[i].name}</h2>`
+                if (team[i].getRole() === "Manager") {
+                    memberCards += `<h2><i class="fas fa-user-tie"></i>`
                 }
-                if (team[i].github) {
-                    memberCards += `<li class="list-group-item">GitHub: <a href="https://github.com/${team[i].github}" target="_blank">${team[i].github}</a></li>`
+                if (team[i].getRole() === "Engineer") {
+                    memberCards += `<h2><i class="fas fa-code-branch"></i>`
                 }
-                if (team[i].school) {
-                    memberCards += `<li class="list-group-item">School: ${team[i].school}</li>`
+                if (team[i].getRole() === "Intern") {
+                    memberCards += `<h2><i class="fas fa-graduation-cap"></i>`
                 }
-                memberCards += `
-                    </div>
-                </div>
+                memberCards += ` ${team[i].getRole()}</h2>
+            </div>
+            <div class="card-bottom list-group">
+                <li>Employee ID: ${team[i].id}</li>
+                <li>Email: <a href="mailto:${team[i].email}">${team[i].email}</a></li>
                 `
-                htmlArray.push(memberCards)
-            }
+        if (team[i].officeNumber) {
+            memberCards += `<li>Office Number: ${team[i].officeNumber}</li>`
+        }
+        if (team[i].github) {
+            memberCards += `<li class="list-group-item">GitHub: <a href="https://github.com/${team[i].github}" target="_blank">${team[i].github}</a></li>`
+        }
+        if (team[i].school) {
+            memberCards += `<li class="list-group-item">School: ${team[i].school}</li>`
+        }
+        memberCards += `
+            </div>
+        </div>
+        `
+        htmlArray.push(memberCards)
+    }
 
+    // HTML BOTTOM
     const htmlEnd = `
             </div>
         </body>
         </html>`
     htmlArray.push(htmlEnd);
 
+    // write file
     fs.writeFile(`./dist/myteam.html`, htmlArray.join(""), function (err) {
         
     })
